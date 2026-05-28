ac374e98bf3cdc53bea4115b8bf7855f419fa348
sz-musialik <280117@student.pwr.edu.pl>
2026-05-28 17:55:50 +0200

    Calculator Fix v1.2

--- utils.py
+++ utils.py
@@ -1,17 +1,21 @@
-# Exemplary calculator functions
+"""Simple calculator module."""
 
 
 def add(a: int, b: int) -> int:
+    """Add two numbers"""
     return a + b
 
 
 def subtract(a: int, b: int) -> int:
+    """Subtract two numbers"""
     return a - b
 
 
 def multiply(a: int, b: int) -> int:
+    """Multiply two numbers"""
     return a * b
 
 
 def divide(a: int, b: int) -> float:
+    """Divide two numbers"""
     return a / b
