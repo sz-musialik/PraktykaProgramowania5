8afaa4d8d11b0b44b4752bf50a2eca723f800d14
sz-musialik <280117@student.pwr.edu.pl>
2026-05-28 17:39:43 +0200

    Added calculator

---
+++ utils.py
@@ -0,0 +1,17 @@
+# Exemplary calculator functions
+
+
+def add(a: int, b: int) -> int:
+    return a + b
+
+
+def subtract(a: int, b: int) -> int:
+    return a - b
+
+
+def multiply(a: int, b: int) -> int:
+    return a * b
+
+
+def divide(a: int, b: int) -> float:
+    return a / b
